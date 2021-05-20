dafa78e24574f47e54d2290d2f0633fbf0ae393c
Sokha Ung <sokhakun@yahoo.com>
2021-05-20 03:16:25 -0700

    Activity Indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = state => {
     return {
@@ -28,6 +29,27 @@ class About extends Component {
             );
         };
 
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title="Community Partner">
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            );
+        }
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title="Community Partner">
+                        <Text>{this.props.partners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            );
+        }
+        
         return (
             <ScrollView>
                 <Mission />
